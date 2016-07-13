0fef3488c5f2e86331f1c3802fff9ab80b808c1a
Ben Wilhelm <benjamin.wilhelm@nytimes.com>
2016-07-13 10:55:40 -0400

    adds TimesCenter assets, music track

--- Assets/EasyMovieTexture/Scripts/MediaPlayerCtrl.cs
+++ Assets/EasyMovieTexture/Scripts/MediaPlayerCtrl.cs
@@ -286,9 +286,23 @@ public class MediaPlayerCtrl : MonoBehaviour
 #endif
 
         m_bInit = true;
-		Play ();
+
+		StartCoroutine(StartVideo ());
     }
 
+	private IEnumerator StartVideo()
+	{
+		yield return new WaitForSeconds (25);
+		AudioSource audioSource = GetComponent<AudioSource> ();
+		float startVolume = audioSource.volume;
+		while (audioSource.volume > 0) {
+			audioSource.volume -= startVolume * Time.deltaTime / 0.5f;
+			yield return null;
+		}
+		audioSource.Stop ();
+		audioSource.volume = startVolume;
+		Play ();
+	}
 
     void OnApplicationQuit()
     {
